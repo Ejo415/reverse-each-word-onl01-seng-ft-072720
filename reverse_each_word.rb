de2a6ed1a26660614b40cedc7996386f7e631e26
Eugene O'Neil <ejo7220@gmail.com>
2020-07-29 21:59:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
-  array.each do |word|
+  array.collect do |word|
     new_array << word.reverse
     
   end
   new_array.join(" ")
-end
